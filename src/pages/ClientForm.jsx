a20bb182e47fd06741c5e1f50db2d47b838ea97c
waleedkhano <khanow99@gmail.com>
2023-12-16 12:58:48 +0500

    verification page bug fixed

--- src/pages/ClientForm.jsx
+++ src/pages/ClientForm.jsx
@@ -4,7 +4,8 @@ import '../css/ClientForm.css';
 import Banner from "../components/Header";
 import Footer from '../components/Footer';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { useNavigate } from "react-router-dom";
 
 const ClientForm = () => {
   const [nombre, setNombre] = useState('');
@@ -21,6 +22,7 @@ const ClientForm = () => {
   const [dniFrente, setDniFrente] = useState(null);
   const [dniDorso, setDniDorso] = useState(null);
   const [retratoDni, setRetratoDni] = useState(null);
+  const navigate = useNavigate();
 
   const handleNombreChange = (e) => {
     setNombre(e.target.value);
@@ -29,39 +31,39 @@ const ClientForm = () => {
   const handleApellidoChange = (e) => {
     setApellido(e.target.value);
   };
-  
+
   const handleCuilChange = (e) => {
     setCuil(e.target.value);
   };
-  
+
   const handleTelefonoChange = (e) => {
     setTelefono(e.target.value);
   };
-  
+
   const handleMailChange = (e) => {
     setMail(e.target.value);
   };
-  
+
   const handleEstadoCivilChange = (e) => {
     setEstadoCivil(e.target.value);
   };
-  
+
   const handleHijosChange = (e) => {
     setHijos(e.target.value);
   };
-  
+
   const handleOcupacionChange = (e) => {
     setOcupacion(e.target.value);
   };
-  
+
   const handleIngresoMensualChange = (e) => {
     setIngresoMensual(e.target.value);
   };
-  
+
   const handleAntiguedadChange = (e) => {
     setAntiguedad(e.target.value);
   };
-  
+
   const handleFechaNacimientoChange = (e) => {
     setFechaNacimiento(e.target.value);
   };
@@ -86,7 +88,7 @@ const ClientForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     // Check if any of the required fields is empty
     if (
       !nombre ||
@@ -108,27 +110,29 @@ const ClientForm = () => {
       alert('Please fill all the fields');
       return;
     }
-  
+
     try {
       // Upload images to Firebase Storage
       const dniFrenteRef = ref(storage, `documentos/${dniFrente.name}`);
       const dniDorsoRef = ref(storage, `documentos/${dniDorso.name}`);
       const retratoDniRef = ref(storage, `documentos/${retratoDni.name}`);
-  
+
       const uploadDniFrenteTask = uploadBytes(dniFrenteRef, dniFrente);
       const uploadDniDorsoTask = uploadBytes(dniDorsoRef, dniDorso);
       const uploadRetratoDniTask = uploadBytes(retratoDniRef, retratoDni);
-  
+
       // Wait for all uploads to complete
       await Promise.all([uploadDniFrenteTask, uploadDniDorsoTask, uploadRetratoDniTask]);
-  
+
       // Get download URLs for the images
       const dniFrenteURL = await getDownloadURL(dniFrenteRef);
       const dniDorsoURL = await getDownloadURL(dniDorsoRef);
       const retratoDniURL = await getDownloadURL(retratoDniRef);
-  
+
       // Save data to Firebase Firestore
       const clientesCollection = collection(db, 'clientes');
+      //added timestamp with form
+      const timestamp = serverTimestamp();
       await addDoc(clientesCollection, {
         nombre,
         apellido,
@@ -145,8 +149,9 @@ const ClientForm = () => {
         dniFrente: dniFrenteURL,
         dniDorso: dniDorsoURL,
         retratoDni: retratoDniURL,
+        timestamp
       });
-  
+
       // Clear all fields after successful submission
       setNombre('');
       setApellido('');
@@ -162,14 +167,16 @@ const ClientForm = () => {
       setDniFrente(null);
       setDniDorso(null);
       setRetratoDni(null);
-  
+
       alert('Data submitted successfully!');
+      navigate("/");
+
     } catch (error) {
       console.error('Error submitting data:', error);
       alert('An error occurred while submitting data');
     }
   };
-  
+
 
   return (
     <div>
@@ -179,92 +186,92 @@ const ClientForm = () => {
       <h1 className='firsth1'>Solicitá tu crédito</h1>
       <form onSubmit={handleSubmit}>
         <div className='form__container' >
-        <div className='form__container__leftpanel'>
-        
+          <div className='form__container__leftpanel'>
+
             <label>
               Nombre:
               <input type="text" value={nombre} onChange={handleNombreChange} />
             </label>
-            
-          
+
+
             <label>
               Apellido:
               <input type="text" value={apellido} onChange={handleApellidoChange} />
             </label>
-          
+
             <label>
               CUIL:
               <input type="text" value={cuil} onChange={handleCuilChange} />
             </label>
-          
+
             <label>
               Teléfono:
               <input type="tel" value={telefono} onChange={handleTelefonoChange} />
             </label>
-          
-          <label>
-            Correo electrónico:
-            <input type="email" value={mail} onChange={handleMailChange} />
-          </label>
-          
-          <label>
-            Estado Civil:
-            <select value={estadoCivil} onChange={handleEstadoCivilChange}>
-              <option value="">-- Seleccione --</option>
-              <option value="soltero">Soltero(a)</option>
-              <option value="casado">Casado(a)</option>
-              <option value="divorciado">Divorciado(a)</option>
-              <option value="viudo">Viudo(a)</option>
-            </select>
-          </label>
-          
-          <label>
-            Hijos:
-            <select value={hijos} onChange={handleHijosChange}>
-              <option value="">-- Seleccione --</option>
-              <option value="true">Si</option>
-              <option value="false">No</option>
-            </select>
-          </label>
-        </div>
-        <div className='form__container__rightpanel'>
-          
-          <label>
-            Ocupación:
-            <input type="text" value={ocupacion} onChange={handleOcupacionChange} />
-          </label>
-        
+
+            <label>
+              Correo electrónico:
+              <input type="email" value={mail} onChange={handleMailChange} />
+            </label>
+
+            <label>
+              Estado Civil:
+              <select value={estadoCivil} onChange={handleEstadoCivilChange}>
+                <option value="">-- Seleccione --</option>
+                <option value="soltero">Soltero(a)</option>
+                <option value="casado">Casado(a)</option>
+                <option value="divorciado">Divorciado(a)</option>
+                <option value="viudo">Viudo(a)</option>
+              </select>
+            </label>
+
+            <label>
+              Hijos:
+              <select value={hijos} onChange={handleHijosChange}>
+                <option value="">-- Seleccione --</option>
+                <option value="true">Si</option>
+                <option value="false">No</option>
+              </select>
+            </label>
+          </div>
+          <div className='form__container__rightpanel'>
+
+            <label>
+              Ocupación:
+              <input type="text" value={ocupacion} onChange={handleOcupacionChange} />
+            </label>
+
             <label>
               Ingreso mensual:
               <input type="number" value={ingresoMensual} onChange={handleIngresoMensualChange} />
             </label>
-            
+
             <label>
               Antigüedad:
               <input type="number" value={antiguedad} onChange={handleAntiguedadChange} />
             </label>
-            
+
             <label>
               Fecha de nacimiento:
               <input type="date" value={fechaNacimiento} onChange={handleFechaNacimientoChange} />
             </label>
-            
-              <label>
-                DNI Frente:
-                <input type="file" onChange={handleDniFrenteChange} />
-              </label>
-            
-              <label>
-                DNI Dorso:
-                <input type="file" onChange={handleDniDorsoChange} />
-              </label>
-            
-              <label>
-                Retrato + DNI:
-                <input type="file" onChange={handleRetratoDniChange} />
-              </label>
-            </div>
+
+            <label>
+              DNI Frente:
+              <input type="file" onChange={handleDniFrenteChange} />
+            </label>
+
+            <label>
+              DNI Dorso:
+              <input type="file" onChange={handleDniDorsoChange} />
+            </label>
+
+            <label>
+              Retrato + DNI:
+              <input type="file" onChange={handleRetratoDniChange} />
+            </label>
           </div>
+        </div>
         <button className='form__btn' type="submit">Enviar</button>
       </form>
 
